2d0f9e333a1623f7049d0d19795426759e7f71bd
naman_samra <naman@tartan.com>
2022-10-09 15:48:00 +0530

    change in server listening

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log('Helo server is running at' + url);
 });
